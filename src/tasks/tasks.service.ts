04c7643a6b3b02674a9b0f685181a208cc135d7c
Giora Guttsait <giora111@gmail.com>
2019-07-16 17:51:29 +0300

    Update GET /tasks/ to return an empty array

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  async getAllTasks(): Promise<any[]> {
+    return [];
+  }
+}
