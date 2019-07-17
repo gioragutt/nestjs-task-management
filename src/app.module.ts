0f62f28d2bfb0511bc41219789a379c7af73f099
Giora Guttsait <giora111@gmail.com>
2019-07-17 14:56:37 +0300

    Add initial auth module code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TYPE_ORM_CONFIG } from './typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule, TypeOrmModule.forRoot(TYPE_ORM_CONFIG)],
+  imports: [TasksModule, TypeOrmModule.forRoot(TYPE_ORM_CONFIG), AuthModule],
 })
 export class AppModule {}
