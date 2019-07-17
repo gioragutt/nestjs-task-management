7db1588988e5878a5e3cd5657723ceffc7a83859
Giora Guttsait <giora111@gmail.com>
2019-07-17 04:14:20 +0300

    Initialize TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TYPE_ORM_CONFIG } from './typeorm.config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, TypeOrmModule.forRoot(TYPE_ORM_CONFIG)],
 })
 export class AppModule {}
