832c01d150fdce264a7deb1470eec0b9e5d5bde4
Giora Guttsait <giora111@gmail.com>
2019-07-18 14:35:41 +0300

    Clean up auth credential validations, add username alphanumeric validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,14 +1,13 @@
-import { MinLength, IsString, MaxLength, Matches } from 'class-validator';
+import { IsString, Matches, Length } from 'class-validator';
 
 export class AuthCredentialsDto {
   @IsString()
-  @MinLength(4)
-  @MaxLength(20)
+  @Length(4, 20)
+  @Matches(/^\w+$/, { message: 'username must contain letters and numbers only' })
   username: string;
 
   @IsString()
-  @MinLength(8)
-  @MaxLength(20)
+  @Length(8, 20)
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message:
       'password must have at least one lowercase letter, one uppercase letter, and at least one number or special character',
