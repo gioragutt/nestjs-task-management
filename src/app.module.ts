ed65c737fb1eb9546f105ae12f20b766ba29533c
Giora Guttsait <giora111@gmail.com>
2019-07-19 14:02:57 +0300

    Add request logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, NestMiddleware } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TYPE_ORM_CONFIG } from './typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { MorganMiddleware } from '@nest-middlewares/morgan';
 
 @Module({
   imports: [TasksModule, TypeOrmModule.forRoot(TYPE_ORM_CONFIG), AuthModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    MorganMiddleware.configure('common');
+    consumer.apply(MorganMiddleware).forRoutes('/');
+  }
+}
